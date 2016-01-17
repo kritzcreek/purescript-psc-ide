06623ff0e5cce57c6e67b1f9fbe5245e2413ff65
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-01-17 16:39:32 +0000

    Handle socket errors

--- src/PscIde.js
+++ src/PscIde.js
@@ -5,15 +5,20 @@ var net = require('net');
 module.exports.send = function(cmd){
   return function(port){
     return function(cb){
-      return function(){
-        var sock = net.createConnection({port: port})
-        sock.on('data', function (data) {
-          cb(data)()
-        })
-        .on('connect', function () {
-          sock.setEncoding('utf8');
-          sock.write(cmd + "\n")
-        })
+      return function(err){
+        return function(){
+          var sock = net.createConnection({port: port})
+          sock.on('data', function (data) {
+            cb(data)()
+          })
+          .on('connect', function () {
+            sock.setEncoding('utf8');
+            sock.write(cmd + "\n")
+          })
+          .on('error', function (e) {
+            err(e)()
+          })
+        }
       }
     }
   }
--- src/PscIde.purs
+++ src/PscIde.purs
@@ -2,6 +2,7 @@ module PscIde where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Aff (Aff, makeAff)
+import Control.Monad.Eff.Exception (Error)
 import Control.Alt ((<|>))
 import Prelude (Unit, (>>>), show, (<$>), pure)
 import Data.Argonaut.Decode (class DecodeJson)
@@ -18,13 +19,13 @@ foreign import send :: forall eff.
   String -> -- ^ Command
   Int ->  -- ^ Port
   (String -> Eff (net :: NET | eff) Unit) -> -- ^ Callback
+  (Error -> Eff (net :: NET | eff) Unit) -> -- ^ Error Callback
   Eff (net :: NET | eff) Unit
-
 
 type Cmd a = forall eff. Aff (net :: NET | eff) (Result a)
 
 sendCommand :: forall i o. (EncodeJson i, DecodeJson o) => i -> Cmd o
-sendCommand c = makeAff (\err succ -> send (show (encodeJson c)) 4242 (unwrapResponse >>> succ))
+sendCommand c = makeAff (\err succ -> send (show (encodeJson c)) 4242 (unwrapResponse >>> succ) err)
 
 cwd :: Cmd Message
 cwd = sendCommand Cwd
