5ed377a7cfecf219fdb864edcd762fd1a9c858ba
Antti Holvikari <anttih@gmail.com>
2016-06-19 12:50:18 +0300

    Update to Aff 1.0.0

--- src/PscIde/Server.purs
+++ src/PscIde/Server.purs
@@ -12,9 +12,9 @@ import Node.Which (which)
 import Control.Alt ((<|>))
 import Control.Monad.Aff (attempt, Aff, later', makeAff)
 import Control.Monad.Aff.AVar (AVAR)
-import Control.Monad.Aff.Par (Par(Par), runPar)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE)
+import Control.Parallel.Class (parallel, runParallel)
 import Node.ChildProcess (CHILD_PROCESS, ChildProcess, StdIOBehaviour,
                          Exit(Normally), onClose, onError, defaultSpawnOptions,
                          spawn, defaultExecOptions, execFile, pipe)
@@ -41,7 +41,7 @@ startServer' stdio exe port projectRoot = do
                                      (Normally n) -> succ $ StartError $ "Error code returned: "<> show n
                                      _ -> succ $ StartError "Other close error")
 
-    runPar (Par handleErr <|> Par (later' 100 $ pure $ Started cp))
+    runParallel (parallel handleErr <|> parallel (later' 100 $ pure $ Started cp))
 
 -- | Stop a psc-ide server.
 stopServer :: forall eff. Int -> Aff (cp :: CHILD_PROCESS, net :: NET | eff) Unit
