fd9061854f2dbee1c002a80c7188b2492f0cae11
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-07-17 14:28:46 +0100

    Add project root command

---
+++ src/PscIde/Project.purs
@@ -0,0 +1,23 @@
+module PscIde.Project where
+
+import Prelude
+import Node.Path as NP
+import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe(..))
+import Node.FS (FS) as FS
+import Node.FS.Sync (exists) as FS
+
+-- | Get PureScript project root given a .purs file (identified by presence of bower.json)
+getRoot :: forall eff'. NP.FilePath -> Eff (fs :: FS.FS | eff') (Maybe NP.FilePath)
+getRoot path =
+  let parent = getParent path
+      bower = NP.concat [path, "bower.json"] in
+  if path == "" || path == parent then
+    pure Nothing
+  else do
+    hasBower <- FS.exists bower
+    if hasBower then pure $ Just path else getRoot parent
+
+  where
+    getParent :: NP.FilePath -> NP.FilePath
+    getParent p = NP.concat [p, ".."]
